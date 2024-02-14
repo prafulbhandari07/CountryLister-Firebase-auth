e0e8e37d4c7beccdcb3809f9b18bb1ed97aaa6fd
Praful Bhandari <pbhandari490@gmail.com>
2024-02-14 10:26:57 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../redux/slices/authSlice";
 import { signOut } from "firebase/auth";
 import { database } from "../firebase";
 
 export default function Header() {
+  const user = useSelector((state) => state.auth.user);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   async function handleLogout() {
-    await signOut(database)
+    await signOut(database);
     dispatch(logout());
     navigate("/");
   }
   return (
     <header>
-      <p>Welcome User</p>
+      <h3>Welcome {user.email}</h3>
       <button id="headerBtn" onClick={handleLogout}>
         Log-out
       </button>
